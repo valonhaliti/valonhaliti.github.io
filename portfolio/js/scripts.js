0354b20119ef99e44167c73700d2351ecb380678
valonhaliti <valonfhaliti@gmail.com>
2016-09-17 00:00:29 +0200

    new changes in html, css and js

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -1,5 +1,17 @@
-jQuery('.skillbar').each(function(){
-    jQuery(this).find('.skillbar-bar').animate({
-        width:jQuery(this).attr('data-percent')
-    },2000);
+
+$(window).scroll(function() {
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = 450;
+
+    if(y_scroll_pos > scroll_pos_test) {
+        jQuery('.skillbar').each(function(){
+            jQuery(this).find('.skillbar-bar').animate({
+                width:jQuery(this).attr('data-percent')
+            },2000);
+        });
+    }
+    else
+    {
+        $("body").css("background-color","#FFF");
+    }
 });
